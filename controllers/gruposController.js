18d809e49953854cf6f49456a08a4a4091bb1e91
adrianmendozatabaz <myamyamya@gmailcom>
2020-05-01 16:15:08 -0500

    Se termino la configuracion para subir archivos al servidor

--- controllers/gruposController.js
+++ controllers/gruposController.js
@@ -1,9 +1,58 @@
 //importaciones
-const Categorias =require('../models/Categorias');
-const Grupos =require('../models/Grupos');
+const Categorias = require('../models/Categorias');
+const Grupos = require('../models/Grupos');
+const multer = require('multer');
+const shortid = require('shortid');
+
+//subir la imagen
+const configuracionMulter = {
+    limits: {
+        fileSize: 100000
+    },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, next) => {
+            next(null, __dirname + '/../public/uploads/grupos/')
+        },
+        filename: (req, file, next) => {
+            const extension = file.mimetype.split('/')[1];
+            next(null, `${shortid.generate()}.${extension}`)
+        }
+    }),
+    fileFilter(req, file, next) {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+            //el formato es valido
+            next(null, true);
+        } else {
+            //el formato no es valido
+            next(new Error('Formato no valido'), false);
+        }
+    }
+}
+
+const upload = multer(configuracionMulter).single('imagen');
+
+exports.subirImagen = (req, res, next) => {
+    upload(req, res, function (error) {
+        if (error) {
+            if (error instanceof multer.MulterError) {
+                if (error.code === 'LIMIT_FILE_SIZE') {
+                    req.flash('error', 'El Archivo es demasiado grande');
+                } else {
+                    req.flash('error', error.message)
+                }
+            } else if (error.hasOwnProperty('message')) {
+                req.flash('error', error.message);
+            }
+            res.redirect('back');
+            return;
+        } else {
+            next();
+        }
+    })
+}
 
 //manda al formulario de nuevo grupo
-exports.formNuevoGrupo = async (req, res) =>{
+exports.formNuevoGrupo = async (req, res) => {
     const categorias = await Categorias.findAll();
 
     res.render('nuevo-grupo', {
@@ -13,11 +62,16 @@ exports.formNuevoGrupo = async (req, res) =>{
 }
 
 // almacena los grupos en la bd
-exports.crearGrupo = async (req, res) =>{
+exports.crearGrupo = async (req, res) => {
     const grupo = req.body;
 
     //almacena el usuario autenticado y la categoria id
     grupo.usuarioId = req.user.id;
+
+    //leer la imagen
+    if (req.file) {
+        grupo.imagen = req.file.filename;
+    }
 
     try {
         await Grupos.create(grupo);
@@ -26,9 +80,8 @@ exports.crearGrupo = async (req, res) =>{
     } catch (error) {
         //estraer los errores
         const erroresSequelize = error.errors.map(err => err.message);
-        
+
         req.flash('error', erroresSequelize);
         res.redirect('/nuevo-grupo');
     }
-    
 }
