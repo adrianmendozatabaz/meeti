05fbece45ed5d2bce930c17464a4a8f472f9487f
Your Name <myamyamya29@gmail.com>
2020-06-10 14:47:27 -0500

    se termino la seccion de comentarios

--- controllers/frontend/comentariosControllerFE.js
+++ controllers/frontend/comentariosControllerFE.js
@@ -1,9 +1,12 @@
 //importacion de los modelos
 const Comentarios = require('../../models/Comentarios');
+const Meeti = require('../../models/Meeti');
 
 exports.agregarComentario = async (req, res, next) => {
 
-    const {comentario} = req.body;
+    const {
+        comentario
+    } = req.body;
 
     //crear comentario en la bd
     await Comentarios.create({
@@ -13,10 +16,47 @@ exports.agregarComentario = async (req, res, next) => {
     });
 
     //redirreccionar al usuario
-    res.redirect('back');    
+    res.redirect('back');
 }
 
 //elimina un comentario de la bd
-exports.eliminarComentario = async (req, res, next) =>{
-    res.send('se elimino');
+exports.eliminarComentario = async (req, res, next) => {
+    //tomar el id del comentario
+    const {
+        comentarioId
+    } = req.body;
+
+    //consultar el comentario
+    const comentario = await Comentarios.findOne({
+        where: {
+            id: comentarioId
+        }
+    })    
+    //verificar si existe el comentario
+    if(!comentario){
+        res.status(404).send('Acción no valida');
+        return next();
+    }
+
+    //consultar el meeti al que pertenece el comentario
+    const meeti = await Meeti.findOne({
+        where: {
+            id: comentario.meetiId
+        }
+    })
+
+    //verificar que el que borra sea el creador
+    if(comentario.usuarioId === req.user.id || meeti.usuarioId === req.user.id){
+        await Comentarios.destroy({
+            where: {
+                id: comentario.id
+            }
+        })
+        res.status(200).send('Eliminado Correctamente');
+        return next();
+    }else{
+        res.status(403).send('Acción no valida');
+        return next();
+    }
+
 }
--- public/js/eliminarComentario.js
+++ public/js/eliminarComentario.js
@@ -5,18 +5,56 @@ document.addEventListener('DOMContentLoaded', () => {
     const formsEliminar = document.querySelectorAll('.eliminar-comentario');
 
     //revisar que existan los comentarios
-    if(formsEliminar.length > 0){
+    if (formsEliminar.length > 0) {
         formsEliminar.forEach(form => {
             form.addEventListener('submit', eliminarComentario);
         })
     }
 });
 
-function eliminarComentario(e){
+function eliminarComentario(e) {
     e.preventDefault();
 
-    axios.post(this.action)
-    .then(respuesta => {
-        console.log(respuesta);        
+    Swal.fire({
+        title: '¿Eliminar comentario?',
+        text: "Un comentario eliminado no se puede recuperar!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, borrar!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.value) {
+
+            // tomar el id del comentario
+            const comentarioId = this.children[0].value;
+
+            //crear el objeto
+            const datos = {
+                comentarioId
+            }
+            //ejecutar axios y pasar los datos
+
+            axios.post(this.action, datos)
+                .then(respuesta => {
+                    console.log(respuesta);
+                    Swal.fire(
+                        'Eliminado!',
+                        respuesta.data,
+                        'success'
+                    )
+                    //elimanar del DOM
+                    this.parentElement.parentElement.remove();
+                }).catch(error => {
+                    if (error.response.status === 403 || error.response.status === 404) {
+                        Swal.fire(
+                            'Error',
+                            error.response.data,
+                            'error'
+                        )
+                    }
+                })
+        }
     })
 }
